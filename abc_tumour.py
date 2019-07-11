f2a07f10be5a091742082359e9fecff1323afa11
Esthy Hung <esther.hung@stcatz.ox.ac.uk>
2019-07-11 14:12:25 +0100

    git add by esthy

--- abc_tumour.py
+++ abc_tumour.py
@@ -1,15 +1,34 @@
 
 import math
-import matplotlib.pylab as pylb
+import matplotlib.pyplot as pyplt
 import numpy as np
-def logistic(k, c_0, r, e, t):
+def logistic(k, c_0, r, sigma, t):
 	"""Simple model of the evolution of number of cancerous cells with time
-	#e. epsilon represents noise."""	#
-	c = (k/(1+(k/(c_0-1))*np.exp(-r*t)) + e
+	k = carrying capacity
+	c_0 = initial count
+	r = rate of growth
+	n epsilon represents noise."""
 
-t = linspace(0, 100, 100)
-s = np.random.normal(mu, sigma, 1000)
+	#t = linspace(0, 5, 100)
+
+	n = np.random.normal(scale=sigma, size=len(t))
+	c = (k / (1 + ((k / c_0) - 1) * np.exp(-r * t))) + n
+	return c
+if __name__ == '__main__':
+	t = np.linspace(0, 5, 100)
+	pyplt.title('Simple model of the evolution of number of cancerous cells with time')
+	pyplt.plot(t, logistic(100, 1, 3, 1, t))
+	pyplt.show()
 
 
-pylb.plot(logistic(1, 100, 45,45, t))
-	pylb.plot(show)
+
+def rms_dev(list_1, list_2):
+	"""General fn for working out standard deviation between two lists that are the same size.
+	Could probs use built in np.std"""
+	diff = []
+	for i in range(len(list_1)):
+		diff.append((list_1[i] - list_2[i])**2)
+
+	st_dev = (sum(diff)/len(list_1))**.5
+	return st_dev
+
